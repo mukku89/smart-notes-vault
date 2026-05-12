37ff16842b222abdd764ee7bbe68de93522af3c6
mukku89 <mukkumussu100@gmail.com>
2026-05-12 18:37:41 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Smart Notes Vault</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark">
+
+<div class="container">
+
+<a class="navbar-brand" href="#">
+Smart Notes Vault
+</a>
+
+<div>
+<a href="register.php" class="btn btn-warning me-2">
+Register
+</a>
+
+<a href="login.php" class="btn btn-light">
+Login
+</a>
+</div>
+
+</div>
+</nav>
+
+<section class="hero">
+
+<div>
+
+<h1>
+Save Your Notes Smartly
+</h1>
+
+<p>
+Add, Read, Edit and Manage Your Study Notes
+</p>
+
+<a href="register.php" class="main-btn">
+Get Started
+</a>
+
+</div>
+
+</section>
+
+<section class="container py-5">
+
+<div class="row">
+
+<div class="col-md-4 mb-4">
+<div class="card p-4 shadow">
+<h3>Add Notes</h3>
+<p>
+Store long study notes easily.
+</p>
+</div>
+</div>
+
+<div class="col-md-4 mb-4">
+<div class="card p-4 shadow">
+<h3>Read Anywhere</h3>
+<p>
+Access your notes anytime.
+</p>
+</div>
+</div>
+
+<div class="col-md-4 mb-4">
+<div class="card p-4 shadow">
+<h3>Search Notes</h3>
+<p>
+Find notes quickly.
+</p>
+</div>
+</div>
+
+</div>
+
+</section>
+
+</body>
+<script src="script.js"></script>
+</html>
